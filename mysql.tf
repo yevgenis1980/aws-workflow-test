3ffadb865775ce005c701af56d230640dd40b17d
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-14 00:34:13 +0200

    update

--- mysql.tf
+++ mysql.tf
@@ -35,7 +35,7 @@ resource "aws_db_instance" "wordpress" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
-  name                 = var.db_name
+  db_name              = var.db_name
   username             = var.db_username
   password             = var.db_password
   publicly_accessible  = false
