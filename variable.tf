c9ee44cb25b19daaea694c171ef7db3d2a4d38ab
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-13 17:14:54 +0200

    update

---
+++ variable.tf
@@ -0,0 +1,20 @@
+# -----------------------------
+# VARIABLES
+# -----------------------------
+variable "db_password" {
+  description = "MySQL root password"
+  type        = string
+  default     = "SuperSecret123!"
+}
+
+variable "db_username" {
+  description = "WordPress MySQL user"
+  type        = string
+  default     = "wordpress_user"
+}
+
+variable "db_name" {
+  description = "WordPress database name"
+  type        = string
+  default     = "wordpress_db"
+}
