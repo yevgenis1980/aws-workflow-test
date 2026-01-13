38ca035691f94b3676d3cceef157f2adfb8a6a1c
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-13 17:38:00 +0200

    update

---
+++ efs.tf
@@ -0,0 +1,19 @@
+# -----------------------------
+# EFS for wp-content
+# -----------------------------
+resource "aws_efs_file_system" "wordpress" {
+  creation_token   = "wordpress-efs"
+  performance_mode = "generalPurpose"
+  encrypted        = true
+
+  tags = {
+    Name = "wordpress-efs"
+  }
+}
+
+resource "aws_efs_mount_target" "wordpress" {
+  for_each = { for idx, subnet in aws_subnet.public : idx => subnet }
+  file_system_id = aws_efs_file_system.wordpress.id
+  subnet_id      = each.value.id
+  security_groups = [aws_security_group.asg_sg.id]
+}
