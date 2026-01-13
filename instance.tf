8ff14263bfff218580db103f90e38e822d8689de
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-13 17:22:21 +0200

    update

--- instance.tf
+++ instance.tf
@@ -1,4 +1,6 @@
-
+# -----------------------------
+# EC2 INSTANCES FOR SCALING
+# -----------------------------
 resource "aws_key_pair" "ubuntu" {
   depends_on = [aws_nat_gateway.nat]
   key_name   = "ubuntu"
@@ -96,6 +98,9 @@ resource "aws_autoscaling_group" "asg" {
   }
 }
 
+# -----------------------------
+# CLOUDWATCH ALARMS FOR SCALING
+# -----------------------------
 resource "aws_autoscaling_policy" "scale_up" {
   name                   = "scale-up"
   autoscaling_group_name = aws_autoscaling_group.asg.name
@@ -111,3 +116,34 @@ resource "aws_autoscaling_policy" "scale_down" {
   scaling_adjustment     = -1
   cooldown               = 300
 }
+
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 70
+  alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_low" {
+  alarm_name          = "cpu-low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 20
+  alarm_actions       = [aws_autoscaling_policy.scale_down.arn]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
+}
+
